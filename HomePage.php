b1bfb23f8b32e394b910b2923efe6a67ffa9c9b3
Sam Rodgers <srodgersns@gmail.com>
2018-11-20 11:16:35 -0600

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,3 +1,110 @@
+<?php  
+session_start();
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$email = $_SESSION["email"];
+  
+$sql = "SELECT email FROM user WHERE email='$email';"; 
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if(mysqli_num_rows($result)<1){
+mysqli_close($conn);
+ header('Location: Login.php');
+exit();
+} 
+
+
+//check if user has admin privilleges
+$sql = "SELECT Admin FROM user WHERE email='$email';";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row["Admin"]=="yes"){
+$admin = "yes";
+}
+
+
+?>
+
+<?php
+//this section is for moving the customers purchases from the product table to the cart table.  It uses a hidden value to track the product name and tracks the quantity value using php variables
+ // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+    }
+
+$error = "";
+
+if(isset($_POST["purchase"])){
+// get the name for the row selected from the hidden field
+$pproduct = $_POST["pname"];
+// get the quantity from the number in the box;
+$pquantity = $_POST["pquantity"];
+
+// This code is used to find the amount of stock for the item selected
+$sqlcheck = "SELECT Quantity FROM Product WHERE ProductTitle='$pproduct';";
+$qcheck = $conn->query($sqlcheck);
+$qrow = $qcheck->fetch_assoc();
+$qres = $qrow["Quantity"];
+
+// This code is used to check that the product is not already in the cart
+$sqlcheck2 = "SELECT ProductTitle FROM cart WHERE ProductTitle='$pproduct' AND user='$email';";
+$ncheck = $conn->query($sqlcheck2);
+$nres = $ncheck->num_rows;
+
+
+// need to check that the quantity is not less than 1 so we are not adding nothing to the cart
+if($pquantity<1)
+{
+$error = "Please specify a quantity";
+echo $error;
+}
+// need to check that the quantity selected is not greater than the amount in stock
+else if($pquantity > $qres)
+{
+$error ="There is not enough stock for this purchase.  Please select an amount lower than the available quantity";
+echo $error;
+}
+// need to check that the item is not already in the customers cart
+else if($nres > 0){
+$error = "This item is already in your cart.  Please first remove it to change your order";
+echo $error;
+}
+
+else{
+
+
+$pprice = $_POST["pcost"];
+
+// add the required values into the users cart
+$sql = "INSERT INTO cart values ('$email', '$pproduct', '$pquantity', '$pprice');";
+
+if($conn->query($sql)===TRUE){
+echo "Product added to cart";
+
+//adjust the quantity in the table to prevent other users from trying to purchase drugs we don't have.  
+$updateq = $qres - $pquantity;
+
+
+$altersql = "UPDATE Product SET Quantity='$updateq' WHERE ProductTitle='$pproduct';";
+$conn->query($altersql);
+
+}
+else{
+echo "error";
+}
+}
+}
+$conn->close();
+
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -19,26 +126,28 @@ table, th, td {
 <header>
 <img src="NIP.jpg" alt="Treee" style = "display:inline" width = "150" height = "150" />
 <p id="titl"><font size="+20">Welcome to Nature in a Pocket (NIP)'s website</font></p>
+
 </header>
-  
-  <div class="search-container">
-    <form action="/action_page.php">
-      <input type="text" placeholder="Search..." name="search">
-      <button type="submit">Submit</button>
-    </form>    
-  </div> 
+  <header class="login">
+ <a href="Cart.php"> Cart</a> <?php if($admin == "yes"){echo "<a href='Admin.php' > Admin </a>";} ?> <a href="Logout.php"> Logout </a>
+</header>
+
+
 
 <div class="vertical-menu">
   <a href="#" class="active">Category</a>
+
+
+
   <?php 		
     // Create connection
-    $conn = new mysqli("localhost", "andriiev", "2006vA3", "andriiev");
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
     }
     
-    $sql = "SELECT Category FROM Categories";
+    $sql = "SELECT Category FROM Product";
    
 
 		$result = $conn->query($sql);
@@ -57,134 +166,42 @@ table, th, td {
 		$conn->close();
 ?>	
 </div>
-<section>
-	
-		<h2 id = "testing">Login</h2>
-		<p>Welcome to NIP! Login please to make any purchases </p>
-		<br/>
-		<form id = "login" onsubmit ="return login()" action="HomePage.php" method="post">
-		<input type="hidden" name="submitted" value="1"/>
-			<table>				
-					<tr><td>Email: </td><td> <input type="text" id="email" name="email" size="30" /></td></tr>					
-				
-					<tr><td>Password: </td><td> <input type="password" id="password" name="password" size="30" /></td></tr>  			
-				<tr>
-		<td>No account? <a href="Registration.php">Sign up</a></td>
-		</tr>
-			</table>	
-		
-		<br/>
-		<br/>
-		
-		<input class = "ordinaryLink submitButton" type = "submit" value = "Login"/>
-		
-		</form>
-		<br/>
-		<br/>
-		<br/>
-	
-</section>
 
 
 
 
-
-
-
-<?php
-$validate = true;
-$reg_Email = "/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/";
-$reg_Pswd = "/^(\S*)?\d+(\S*)?$/";
-
-$email = "";
-$error = "";
-
-if (isset($_POST["submitted"]) && $_POST["submitted"])
-{
-	$email = trim($_POST["email"]);
-	$password = trim($_POST["password"]);
-
-	$db = new mysqli("localhost", "andriiev", "2006vA3", "andriiev");
-	if ($db->connect_error)
-	{
-		die ("Connection failed: " . $db->connect_error);
-	}
-
-	//add code here to select * from table User where email = '$email' AND password = '$password'
-	// start with
-	$q = "select * from OnlineStore where email = '$email' and password = '$password'";
-	 
-	$r = $db->query($q);
-	$row = $r->fetch_assoc();
-	if($email != $row["email"] && $password != $row["password"])
-	{
-		$validate = false;
-	}
-	else
-	{
-		$emailMatch = preg_match($reg_Email, $email);
-		if($email == null || $email == "" || $emailMatch == false)
-		{
-			$validate = false;
-		}
-
-		$pswdLen = strlen($password);
-		$passwordMatch = preg_match($reg_Pswd, $password);
-		if($password == null || $password == "" || $pswdLen < 8 || $passwordMatch == false)
-		{
-			$validate = false;
-		}
-	}
-
-	if($validate == true)
-	{
-		if($email == "admin@uregina.ca")
-		{
-		session_start();
-		$_SESSION["email"] = $row["email"];
-		header("Location: Admin.php");
-		$db->close();
-		exit();
-		}
-		else
-		{
-			session_start();
-			$_SESSION["email"] = $row["email"];
-			header("Location: Profile.php");
-			$db->close();
-			exit();
-		}
-	}
-	else
-	{
-		$error = "The email/password combination was incorrect. Login failed.";
-		echo "<script type='text/javascript'>alert('$error');</script>";
-		$db->close();
-	}
-}
-
-?>
 <div id ="home" class="LIST">
 <?php 		
     // Create connection
-    $conn = new mysqli("localhost", "andriiev", "2006vA3", "andriiev");
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
     }
     
-    $sql = "SELECT ProductTitle, Category,  Quontity FROM Product";
-   
-
+    $sql = "SELECT ProductTitle, Category,  Quantity, Price FROM Product ORDER BY Category";
+   //   $sql = "SELECT * FROM PRODUCT";
+ 
 		$result = $conn->query($sql);
 		
 		if ($result->num_rows > 0) {
-		    echo "<table><tr><th>Product Name</th><th>Category</th><th>Quontity</th></tr>";
+		// start the form and table
+		
+		    echo "<center><table><tr><th>Product Name</th><th>Category</th><th>Quantity</th><th>Price</th></tr>";
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
-		        echo "<tr><td>" . $row["ProductTitle"]. "</td><td>" . $row["Category"]. "</td><td>" . $row["Quontity"]. "</td></tr>";
-		    }
-		    echo "</table>";
+			echo "<form method='post' action='HomePage.php'>";
+		
+		        echo "<tr><td>" . $row["ProductTitle"]. "</td><td>" . $row["Category"]. "</td><td>" . $row["Quantity"]. "</td><td>" . $row["Price"] . "</td>";
+		
+			echo "<td><input type='number' value='0' name='pquantity'>";
+			echo " <input type='submit' value='add to cart'  name='purchase'>";
+			echo "<input type='hidden' value=\"$row[ProductTitle]\" name='pname'>";
+			echo "<input type='hidden' value=\"$row[Price]\" name='pcost'> </td></tr>";
+	echo "</form>";		
+    }
+		    echo "</table></center>";
+		
 		} else {
 		    echo "0 results";
 		}
@@ -192,12 +209,11 @@ if (isset($_POST["submitted"]) && $_POST["submitted"])
 		$conn->close();
 ?>
 </div>
-	<footer>
-		Validate <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Furegina.ca%2F~martinsp%2F" class = "ordinaryLink">HTML5</a>
-	</footer>
+
    
 <footer> © Nature In a Pocket 2018 </footer>
 <script type = "text/javascript"  src = "validation.js" ></script>
 </body>
 
 </html>
+
